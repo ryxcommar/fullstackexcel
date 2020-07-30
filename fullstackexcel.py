f6683631425c0ee829d155b021267bb5cdca2a2d
ryxcommar <ryxcommar@gmail.com>
2020-07-30 00:15:30 -0400

    added jinja template support

--- fullstackexcel.py
+++ fullstackexcel.py
@@ -7,8 +7,11 @@ import pandas as pd
 import click
 
 from flask import Flask
+from flask import current_app
 from flask.cli import FlaskGroup
 from flask.cli import run_command
+
+from jinja2 import DictLoader
 
 
 def get_routes_from_wb(excel_file: str) -> List[Dict[str, str]]:
@@ -17,16 +20,35 @@ def get_routes_from_wb(excel_file: str) -> List[Dict[str, str]]:
     return routes.to_dict(orient='records')
 
 
-def create_html_from_sheet(excel_file: str, sheet_name: str) -> str:
+def get_templates_from_wb(excel_file: str) -> List[str]:
+    templates = pd.read_excel(excel_file, sheet_name='!templates', header=None)
+    return list(templates[0])
+
+
+def get_html_from_sheet(excel_file: str, sheet_name: str) -> str:
     df_html = pd.read_excel(excel_file, sheet_name=sheet_name, header=None)
     df_html = df_html.fillna('').astype(str)
-    return '\n'.join(list(df_html.sum(axis=1)))
+    html = '\n'.join(list(df_html.sum(axis=1)))
+    return html
+
+
+def render_html_from_sheet(excel_file: str, sheet_name: str) -> str:
+    html = get_html_from_sheet(excel_file, sheet_name)
+    return current_app.jinja_env.from_string(html).render()
 
 
 def create_routing_func(excel_file: str, sheet_name: str) -> callable:
-    _f = lambda: create_html_from_sheet(excel_file, sheet_name)
+    _f = lambda: render_html_from_sheet(excel_file, sheet_name)
     _f.__name__ = re.sub(r'\W+', '', sheet_name.lower())
     return _f
+
+
+def create_jinja_env(app: Flask, excel_file: str):
+    templates = get_templates_from_wb(excel_file)
+    app.jinja_env.loader = DictLoader({
+        template: get_html_from_sheet(excel_file, template)
+        for template in templates
+    })
 
 
 def create_app(excel_file: str = None) -> Flask:
@@ -37,6 +59,8 @@ def create_app(excel_file: str = None) -> Flask:
         for routing_rule in get_routes_from_wb(excel_file):
             f = create_routing_func(excel_file, routing_rule['sheet_name'])
             app.route(routing_rule['route'])(f)
+
+        create_jinja_env(app, excel_file)
 
     return app
 
@@ -69,10 +93,23 @@ def create_demo():
         routes = [['/', 'hello_world'], ['/foo', 'foo']]
         pd.DataFrame(routes).to_excel(sheet_name='!routes', **kwargs)
 
+        templates = [['example_template']]
+        pd.DataFrame(templates).to_excel(sheet_name='!templates', **kwargs)
+
+        example_template = [
+            ['<head>', None, None, None],
+            [None, '<title>', 'Fullstack with Excel', '</title>'],
+            ['</head>', None, None, None],
+            ['{% block content %}', None, None, None],
+            ['{% endblock %}', None, None, None]
+        ]
+        pd.DataFrame(example_template).to_excel(sheet_name='example_template', **kwargs)
+
         hello_world_page = [
-            ['<head>', '<title>full stack with excel</title>', None],
-            ['</head>', None, None],
-            ['<b>', 'hello, world!', '</b>']
+            ['{% extends "example_template" %}', None, None],
+            ['{% block content %}', None, None],
+            ['<b>', 'hello, world!', '</b>'],
+            ['{% endblock %}', None, None]
         ]
         pd.DataFrame(hello_world_page).to_excel(sheet_name='hello_world', **kwargs)
 
