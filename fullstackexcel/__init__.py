47677c924cd5bee37a2fe871f49b32fe069231a4
ryxcommar <ryxcommar@gmail.com>
2020-08-02 11:22:36 -0400

    versioning system

--- fullstackexcel/__init__.py
+++ fullstackexcel/__init__.py
@@ -1,2 +1,4 @@
 from .core import create_app
 from .core import cli
+
+from ._version import VERSION as __version__
--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
+import re
 from setuptools import setup
+
+
+with open('README.md', 'r', encoding='utf8') as f:
+    readme = f.read()
+
+
+with open('fullstackexcel/_version.py', 'r', encoding='utf8') as f:
+    version = re.match("VERSION = '(.*?)'", f.read())[1]
+
 
 setup(
     name='fullstackexcel',
-    version='0.0.1',
+    version=version,
     py_modules=['fullstackexcel'],
     author='@ryxcommar',
+    maintainer='@ryxcommar',
     url='https://twitter.com/ryxcommar',
     entry_points={
         'console_scripts': ['fse=fullstackexcel:cli']
     },
-    install_requires=['flask', 'pandas', 'xlrd', 'xlsxwriter']
+    install_requires=['flask', 'pandas', 'xlrd', 'xlsxwriter'],
+    long_description=readme,
+    python_requires='>=3.6'
 )
